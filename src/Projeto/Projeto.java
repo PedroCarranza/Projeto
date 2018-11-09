528b612c21b83c22582f27c201d6562ebd036273
Henrique <Henrique@PC-Henrique>
2018-11-09 17:41:43 -0200

    commit frame

--- src/Projeto/Projeto.java
+++ src/Projeto/Projeto.java
@@ -1,9 +1,15 @@
 package Projeto;
 
+import javax.swing.JFrame;
+
 public class Projeto {
     
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame();
+        frame.setSize(1280,720);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(3);
+        frame.setLocationRelativeTo(null);
     }
     
 }
