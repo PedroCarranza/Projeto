67d8ddae917f595ed4054a46e01f7b48a074b9d2
PedroCarranza <16tnRvewggPA>
2018-11-19 11:08:56 -0200

    Update src/Projeto/Menu.java

--- src/Projeto/Menu.java
+++ src/Projeto/Menu.java
@@ -7,13 +7,19 @@ import java.awt.Rectangle;
 
 public class Menu {
 
-    public static void drawMenu(Graphics2D g, Projeto pr) {
+    Projeto pr;
 
-        Rectangle b1 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
-        Rectangle b2 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 2 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
-        Rectangle b3 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 3 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
-        Rectangle b4 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 4 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
-        Rectangle b5 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 5 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+    public Menu(Projeto pj) {
+        pr = pj;
+    }
+
+    Rectangle b1 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+    Rectangle b2 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 2 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+    Rectangle b3 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 3 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+    Rectangle b4 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 4 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+    Rectangle b5 = new Rectangle(pr.getWidth() / 2 - pr.getWidth() / 8, 5 * pr.getHeight() / 8, pr.getWidth() / 4, pr.getWidth() / 32);
+
+    public void drawMenu(Graphics2D g, Projeto pr) {
 
         FontMetrics fonte = g.getFontMetrics(g.getFont());
 
