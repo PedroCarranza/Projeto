514131769ba2662ab3d1b81182d0f38566b3cec4
Henrique <Henrique@PC-Henrique>
2018-11-19 12:17:57 -0200

    player class added

--- src/Projeto/Tela.java
+++ src/Projeto/Tela.java
@@ -2,30 +2,20 @@ package Projeto;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class Tela {
 
     Projeto pr;
     Menu men;
-    BufferedImage sprites;
-    int p1Pos[];
-    int frame = 0, moveSpeed = 5;
+    player p1;
+
+    
 
     public Tela(Projeto p) {
         pr = p;
-        p1Pos = new int[2];
-        p1Pos[0] = 20;
-        p1Pos[1] = 360;
         men = new Menu(pr);
-        try {
-            sprites = ImageIO.read(new File("spsheet.png"));
-        } catch (IOException e) {
-            System.err.println("Imagem não encontrada");
-        }
+        p1 = new player(p);
+        
     }
 
     public void updatePaused() {
@@ -39,45 +29,11 @@ public class Tela {
 
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, pr.getWidth(), pr.getHeight());
-        if (pr.lis.right && p1Pos[0] + 100 + moveSpeed < pr.getWidth()) {
-            p1Pos[0] += moveSpeed;
-        }
-        if (pr.lis.left && p1Pos[0] - moveSpeed > 0) {
-            p1Pos[0] -= moveSpeed;
-        }
-        if (pr.lis.down && p1Pos[1] + 39 + moveSpeed < pr.getHeight()) {
-            p1Pos[1] += moveSpeed;
-        }
-        if (pr.lis.up && p1Pos[1] - moveSpeed > 0) {
-            p1Pos[1] -= moveSpeed;
-        }
-
-        frame++;
-        if (frame > 9) {
-            frame = 0;
-        }
 
         g.setColor(Color.red);
         g.drawOval(pr.lis.mx - 50, pr.lis.my - 50, 100, 100);
-        if (pr.lis.up && !pr.lis.down) {
-            if (!pr.lis.left) {
-                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 5, 43, 35), p1Pos[0], p1Pos[1], 43 * 2, 35 * 2, null);
-            } else {
-                g.drawImage(sprites.getSubimage(321, 5, 36, 35), p1Pos[0] + 12, p1Pos[1], 36 * 2, 35 * 2, null);
-            }
-        } else if (!pr.lis.up && pr.lis.down) {
-            if (!pr.lis.left) {
-                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 89, 43, 39), p1Pos[0], p1Pos[1], 43 * 2, 39 * 2, null);
-            } else {
-                g.drawImage(sprites.getSubimage(321, 89, 36, 39), p1Pos[0] + 12, p1Pos[1], 36 * 2, 39 * 2, null);
-            }
-        } else {
-            if (!pr.lis.left) {
-                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 42, 43, 39), p1Pos[0], p1Pos[1], 43 * 2, 39 * 2, null);
-            } else {
-                g.drawImage(sprites.getSubimage(321, 42, 36, 39), p1Pos[0] + 12, p1Pos[1], 36 * 2, 39 * 2, null);
-            }
-        }
+        
+        p1.updatePlayer(g);
 
         g.dispose();
     }
--- src/Projeto/player.java
+++ src/Projeto/player.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Projeto;
 
-/**
- *
- * @author Henrique
- */
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class player {
+    int p1Pos[];
+    Projeto pr;
+    BufferedImage sprites;
+    int frame = 0, moveSpeed = 5;
+    public player(Projeto p){
+        pr = p;
+        p1Pos = new int[2];
+        p1Pos[0] = 20;
+        p1Pos[1] = 360;
+        try {
+            sprites = ImageIO.read(new File("spsheet.png"));
+        } catch (IOException e) {
+            System.err.println("Imagem não encontrada");
+        }
+    }
     
+    public void updatePlayer(Graphics2D g){
+        frame++;
+        if (frame > 9) {
+            frame = 0;
+        }
+        if (pr.lis.right && p1Pos[0] + 100 + moveSpeed < pr.getWidth()) {
+            p1Pos[0] += moveSpeed;
+        }
+        if (pr.lis.left && p1Pos[0] - moveSpeed > 0) {
+            p1Pos[0] -= moveSpeed;
+        }
+        if (pr.lis.down && p1Pos[1] + 78 + moveSpeed < pr.getHeight()) {
+            p1Pos[1] += moveSpeed;
+        }
+        if (pr.lis.up && p1Pos[1] - moveSpeed > 0) {
+            p1Pos[1] -= moveSpeed;
+        }
+        if (pr.lis.up && !pr.lis.down) {
+            if (!pr.lis.left) {
+                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 5, 43, 35), p1Pos[0], p1Pos[1], 43 * 2, 35 * 2, null);
+            } else {
+                g.drawImage(sprites.getSubimage(321, 5, 36, 35), p1Pos[0] + 12, p1Pos[1], 36 * 2, 35 * 2, null);
+            }
+        } else if (!pr.lis.up && pr.lis.down) {
+            if (!pr.lis.left) {
+                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 89, 43, 39), p1Pos[0], p1Pos[1], 43 * 2, 39 * 2, null);
+            } else {
+                g.drawImage(sprites.getSubimage(321, 89, 36, 39), p1Pos[0] + 12, p1Pos[1], 36 * 2, 39 * 2, null);
+            }
+        } else {
+            if (!pr.lis.left) {
+                g.drawImage(sprites.getSubimage(229 + 46 * (frame / 5), 42, 43, 39), p1Pos[0], p1Pos[1], 43 * 2, 39 * 2, null);
+            } else {
+                g.drawImage(sprites.getSubimage(321, 42, 36, 39), p1Pos[0] + 12, p1Pos[1], 36 * 2, 39 * 2, null);
+            }
+        }
+    }
 }
