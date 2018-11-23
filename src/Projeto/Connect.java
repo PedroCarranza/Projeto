1ddb5af6dad2960f4bd8537a754b83e5fe3af2a0
PedroCarranza <16tnRvewggPA>
2018-11-23 12:43:21 -0200

    Tirando autor

--- src/Projeto/Connect.java
+++ src/Projeto/Connect.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Projeto;
 
 import java.io.IOException;
@@ -15,10 +10,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Enumeration;
 
-/**
- *
- * @author Henrique
- */
 public class Connect extends Thread implements Runnable {
 
     int opt;
--- src/Projeto/Inimigo.java
+++ src/Projeto/Inimigo.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Projeto;
 
 import java.awt.Graphics2D;
@@ -13,10 +8,6 @@ import java.io.IOException;
 import java.time.Instant;
 import javax.imageio.ImageIO;
 
-/**
- *
- * @author Henrique
- */
 public class Inimigo {
     int px,py;
     int hp = 3;
